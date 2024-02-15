1cc3c36bdaf30f37e7e50a5914c4335849652106
AkhmedovDev <152636243+Akhmedov-Dev@users.noreply.github.com>
2024-02-15 17:58:20 +0500

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1ZQ00jFjKdDzDE_nqK6gsCX0E38LZQbc",
-  authDomain: "first-project-24-group.firebaseapp.com",
-  projectId: "first-project-24-group",
-  storageBucket: "first-project-24-group.appspot.com",
-  messagingSenderId: "233578948880",
-  appId: "1:233578948880:web:753a850678bc253df65d95",
-  measurementId: "G-QFKKXF7VL1"
+  apiKey: "AIzaSyAfpg6cR4le6Y0xDykImldDb5IGbYbVABM",
+  authDomain: "homework-6a4c7.firebaseapp.com",
+  projectId: "homework-6a4c7",
+  storageBucket: "homework-6a4c7.appspot.com",
+  messagingSenderId: "945788651445",
+  appId: "1:945788651445:web:61fdc9afb582ee7aebbbfc",
+  measurementId: "G-M9K13GXKYG"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
